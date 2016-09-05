84926cebf107a3a5c394e05593a2da794a03c7cf
ineedblood <fernandou@google.com>
2016-09-05 12:54:35 +0200

    Create postfix-yara.py

---
+++ postfix-yara.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+
+import sys
+import email
+import yara
+import os
+
+RULESET_PATH = '/home/usr/rulesets/'
+
+CORES = 1
+
+EX_TEMP_FAIL = 75
+EX_UNAVAILABLE = 69
+SUCCESS = 0
+
+detections = []
+
+
+def process_payload(payload, ruleset):
+  ruleset_name = ruleset
+  ruleset = yara.load(ruleset)
+  if ruleset.match(data=str(payload)):
+    detections.append(ruleset_name)
+
+def main():
+  ruleset_list = json.load(open(RULESET_PATH + 'rulesets.json', 'rb'))
+  mail = email.message_from_file(sys.stdin)
+  for part in mail.walk():
+    for ruleset in ruleset_list:
+      process_payload(part.get_payload(decode=True), ruleset['name'])
+    if len(detections) > 0:
+      return EX_TEMP_FAIL
+  return SUCCESS
+
+if __name__ == '__main__':
+  main()
+
